e3501c7e05763a3d54867b06d45b85e5cc8ac7d9
Ryan Tartar <ryantartar32@gmail.com>
2021-04-12 16:10:31 -0600

    fixed project page for small screens

--- src/components/Project.js
+++ src/components/Project.js
@@ -25,7 +25,7 @@ export default function Project() {
             <section className="container mx-auto">
                 <h1 className="text-5xl flex justify-center cursive">My Projects</h1>
                 <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my Projects</h2>
-               <section className="grid grid-cols-2 gap-8">
+               <section className="grid grid-cols-2 gap-8 absolute">
                     {projectData && projectData.map((project, index) => (
                     <article className="relative rounded-lg shadow-xl bg-green-300 p-16">
                         <h3 className="text-gray-800 text-3xl font-bold mb-12 hover:text-red-700">
